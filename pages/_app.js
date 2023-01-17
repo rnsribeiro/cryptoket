a02a1ff4b2c48a09cd2d090f2a9bb6f8026beb12
Rodrigo Nunes <rnsribeiro@gmail.com>
2023-01-17 08:11:04 -0300

    Corrigido alguns erros

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+
+import { Navbar, Footer } from '../components'
 import '../styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className='dark:bg-nft-dark bg-white 
+                    min-h-screen'>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+
+    <Script src="https://kit.fontawesome.com/7955880019.js" crossOrigin="anonymous" />
+  </ThemeProvider>
+)
+
+export default MyApp;
