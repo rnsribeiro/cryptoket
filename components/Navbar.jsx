1f9ef06ba3ca9c22ddc3afda42f4323a57b4ae04
Rodrigo Nunes <rnsribeiro@gmail.com>
2023-01-18 22:44:47 -0300

    Define Theme Dark and Light and Menu of the navigation.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,8 +6,41 @@ import Link from 'next/link';
 
 import images from '../assets';
 
+const MenuItems = ({ isMobile, active, setActive }) => {
+    const generateLink = (i) => {
+        switch (i) {
+            case 0: return '/';
+            case 1: return '/created-nfts';
+            case 2: return '/my-nfts';
+            default: return '/';
+        }
+    };
+
+    return (
+        <ul className={`list-none flexCenter flex-row ${isMobile && 'flex-col h-full'}`}>
+            {['Explore NFTs', 'Listed NFTs', 'My NFTs'].map((item, i) => (
+                <li
+                    key={i}
+                    onClick={() => {
+                        setActive(item);
+                    }}
+                    className={`flex flex-row items-center font-poppins font-semibold 
+                    text-base dark:hover:text-white hover:text-nft-dark mx-3
+                    ${active === item 
+                    ? 'dark:text-white  text-nft-black-1'
+                    : 'dark:text-nft-gray-3 text-nft-gray-2'}
+                    `}
+                >
+                    <Link href={generateLink(i)}>{item}</Link>
+                </li>
+            ))}
+        </ul>
+    );
+};
+
 const Navbar = () => {
     const { theme, setTheme } = useTheme();
+    const [ active, setActive ] = useState('Explore NFTs')
 
     return (
         <nav className='flexBetween w-full fixed z-10 p-4 
@@ -29,6 +62,23 @@ const Navbar = () => {
                         objectFit="contain" width={32} height={32} alt="logo" />
                     </div>
                 </Link>
+            </div>
+            <div className='flex flex-initial flex-row justify-end'>
+                <div className='flex items-center mr-2'>
+                    <input type="checkbox" className="checkbox" id="checkbox"
+                        onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')} />
+                    <label htmlFor="checkbox" className='flexBetween w-8 h-4 bg-black
+                        rounded-2xl p-1 relative label'>
+                        <i className='fas fa-sun' />
+                        <i className='fas fa-moon' />
+                        <div className='w-3 h-3 absolute bg-white rounded-full ball' ></div>
+                    </label>
+                </div>
+            </div>
+            <div className='md:hidden flex'>
+                <ul className='list-none flexCenter flex-row'>
+                    <MenuItems active={active} setActive={setActive} />
+                </ul>
             </div>
         </nav>        
     )
