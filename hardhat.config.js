951cac317e46f2073a3c12f64faf12e7f30eb73c
Rodrigo Nunes <rnsribeiro@gmail.com>
2023-02-08 22:48:26 -0300

    Alterar gitignore

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 require('@nomiclabs/hardhat-waffle');
 
 const privateKey = fs.readFileSync('.secret').toString().trim();
+const ALCHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY;
+const GOERLI_PRIVATE_KEY = process.env.NEXT_PUBLIC_GOERLI_PRIVATE_KEY;
 
 module.exports = {
   networks: {
@@ -12,6 +14,10 @@ module.exports = {
     mumbai: {
       url: 'https://rpc-mumbai.maticvigil.com',
       accounts: [privateKey],
+    },
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
     },
     // rinkeby: {
     // url: 'https://rinkeby.infura.io/v3/bed4fdcc76bb4978a9a3103ef0946f64',
